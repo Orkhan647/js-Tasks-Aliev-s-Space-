277809861263653185ed3b53b6eadd96ca76da7b
Orkhan647 <okuw.k@mail.ru>
2026-05-23 12:30:33 +0400

    js Task 11 completed

---
+++ jsTask11/app.js
@@ -0,0 +1,20 @@
+function edediTersCevir(number) {
+    if (number < 0) {
+        return "Xəta: Mənfi ədəd daxil edilə bilməz!";
+    }
+
+    let numberToStr = String(number);
+    let reverseStr = "";
+
+    for (let i = numberToStr.length - 1; i >= 0; i--) {
+        reverseStr += numberToStr[i];
+    }
+
+    return +reverseStr; 
+}
+
+let number = +prompt("eded daxil edin");
+let netice = edediTersCevir(number);
+
+
+alert(`Ters eded: ${netice}`);  
