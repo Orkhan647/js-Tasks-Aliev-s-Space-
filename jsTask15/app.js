7cbea87c1d5943f1877e2c6c7247d9f73187c6e1
Orkhan647 <okuw.k@mail.ru>
2026-05-31 12:18:21 +0400

    Js Task 15 copleted

---
+++ jsTask15/app.js
@@ -0,0 +1,28 @@
+// 2. Maaşdan vergi çıxma
+// Funksiya yaz:
+// maaş qəbul etsin
+// 13% vergi çıx
+// net maaşı qaytar
+
+const calculateSalary = (salary) => {
+    
+  const calculateTax = (amount) => amount * 0.13;
+
+  const netSalary = salary - calculateTax(salary);
+
+  return netSalary;
+};
+
+let salaryInput = +prompt("Maasinizi daxil edin");
+
+while (isNaN(salaryInput) || salaryInput < 0) {
+  alert("Xeta: maasinizi duzgun daxil edin");
+
+  salaryInput = +prompt("Maasinizi daxil edin");
+}
+
+let netSalary = calculateSalary(salaryInput);
+
+alert(
+  `Maasiniz: ${salaryInput} AZN \nVergiden sonra Maasiniz: ${netSalary} AZN`,
+);
