b86d884b2d024aab1b9916e8b8ce14689953d35f
Orkhan647 <okuw.k@mail.ru>
2026-07-09 19:06:18 +0400

    complete task todo

---
+++ jsTask(ToDo)/app.js
@@ -0,0 +1,129 @@
+const title = document.getElementById("title");
+const description = document.getElementById("description");
+const addBtn = document.getElementById("addBtn");
+const todoList = document.getElementById("todoList");
+
+const modal = document.getElementById("modal");
+const editTitle = document.getElementById("editTitle");
+const editDescription = document.getElementById("editDescription");
+const saveBtn = document.getElementById("saveBtn");
+const cancelBtn = document.getElementById("cancelBtn");
+const clearBtn = document.getElementById("clearBtn");
+
+let todos = JSON.parse(localStorage.getItem("todos")) || [];
+let editIndex;
+
+function render() {
+  todoList.innerHTML = "";
+
+  todos.forEach((todo, index) => {
+    todoList.innerHTML += `
+        <div class="todo ${todo.completed ? "completed" : ""} " data-id="${index}">
+            <div class="left">
+                <h2>${todo.title}</h2>
+                <p>${todo.description}</p>
+            </div>
+
+            <div class="right">
+                <button class="edit">Edit</button>
+                <button class="delete">Delete</button>
+            </div>
+        </div>
+        `;
+  });
+
+  localStorage.setItem("todos", JSON.stringify(todos));
+}
+
+render();
+
+// Add Todo
+addBtn.addEventListener("click", () => {
+  if (!title.value.trim() || !description.value.trim()) {
+    alert("Bütün xanalari doldurun");
+    return;
+  }
+
+  todos.push({
+    title: title.value,
+    description: description.value,
+    completed: false,
+  });
+
+  title.value = "";
+  description.value = "";
+
+  render();
+});
+
+// Event Delegation
+todoList.addEventListener("click", function (e) {
+  const todo = e.target.closest(".todo");
+
+  if (!todo) return;
+
+  const index = todo.dataset.id;
+
+  // Delete
+  if (e.target.classList.contains("delete")) {
+    todos.splice(index, 1);
+    render();
+  }
+
+  // Edit
+  else if (e.target.classList.contains("edit")) {
+    editIndex = index;
+
+    editTitle.value = todos[index].title;
+    editDescription.value = todos[index].description;
+
+    modal.style.display = "flex";
+  }
+
+  // Complete
+  else if (e.target.closest(".left")) {
+    todos[index].completed = !todos[index].completed;
+
+    render();
+  }
+});
+
+// Save Edit
+saveBtn.addEventListener("click", () => {
+  if (!editTitle.value.trim() || !editDescription.value.trim()) {
+    alert("Boş buraxmaq olmaz");
+    return;
+  }
+
+  todos[editIndex].title = editTitle.value;
+  todos[editIndex].description = editDescription.value;
+
+  modal.style.display = "none";
+
+  render();
+});
+
+// Cancel
+cancelBtn.addEventListener("click", () => {
+  modal.style.display = "none";
+});
+
+// Modal baglama
+window.addEventListener("click", (e) => {
+  if (e.target === modal) {
+    modal.style.display = "none";
+  }
+});
+
+clearBtn.addEventListener("click", function () {
+  if (todos.length == 0) {
+    alert("Todo yoxdur.");
+    return;
+  }
+
+  if (confirm("Bütün tasklar silinsin?")) {
+    todos = [];
+
+    render();
+  }
+});
--- jsTask19(form)/app.js
+++ jsTask19(form)/app.js
@@ -1,21 +1,53 @@
 const orderForm = document.getElementById("orderForm");
 
 const fields = [
-  { input: document.getElementById("product"), error: document.getElementById("product-error") },
-  { input: document.getElementById("quantity"), error: document.getElementById("quantity-error") },
-  { input: document.getElementById("delivery-date"), error: document.getElementById("delivery-date-error") },
-  { input: document.getElementById("name"), error: document.getElementById("name-error") },
-  { input: document.getElementById("email"), error: document.getElementById("email-error") },
-  { input: document.getElementById("phone"), error: document.getElementById("phone-error") },
-  { input: document.getElementById("terms"), error: document.getElementById("terms-error"), isCheckbox: true },
+  {
+    input: document.getElementById("product"),
+    error: document.getElementById("product-error"),
+  },
+  {
+    input: document.getElementById("quantity"),
+    error: document.getElementById("quantity-error"),
+  },
+  {
+    input: document.getElementById("delivery-date"),
+    error: document.getElementById("delivery-date-error"),
+  },
+  {
+    input: document.getElementById("name"),
+    error: document.getElementById("name-error"),
+  },
+  {
+    input: document.getElementById("email"),
+    error: document.getElementById("email-error"),
+  },
+  {
+    input: document.getElementById("phone"),
+    error: document.getElementById("phone-error"),
+  },
+  {
+    input: document.getElementById("terms"),
+    error: document.getElementById("terms-error"),
+    isCheckbox: true,
+  },
 ];
 
-const [productXana, quantityXana, dateXana, nameXana, emailXana, phoneXana, termsXana] = fields;
+const [
+  productXana,
+  quantityXana,
+  dateXana,
+  nameXana,
+  emailXana,
+  phoneXana,
+  termsXana,
+] = fields;
 
 // 1. input check
 fields.forEach(function (xana) {
   xana.input.addEventListener("blur", function () {
-    let bosdur = xana.isCheckbox ? xana.input.checked === false : xana.input.value === "";
+    let bosdur = xana.isCheckbox
+      ? xana.input.checked === false
+      : xana.input.value === "";
 
     if (bosdur) {
       xana.error.style.display = "block";
@@ -25,10 +57,6 @@ fields.forEach(function (xana) {
   });
 });
 
-
-
-
-
 // 2. submit check
 orderForm.addEventListener("submit", function (e) {
   e.preventDefault();
@@ -36,41 +64,37 @@ orderForm.addEventListener("submit", function (e) {
   let formDüzgündür = true;
 
   fields.forEach(function (xana) {
-    let bosdur = xana.isCheckbox ? xana.input.checked === false : xana.input.value === "";
+    let bosdur = xana.isCheckbox
+      ? xana.input.checked === false
+      : xana.input.value === "";
 
     if (bosdur) {
       xana.error.style.display = "block";
-      formDüzgündür = false; 
+      formDüzgündür = false;
     } else {
       xana.error.style.display = "none";
     }
   });
 
-
-
-
-
-    //delivery time check
-  const secilenSaat = document.querySelector('input[name="delivery_time"]:checked');
+  //delivery time check
+  const secilenSaat = document.querySelector(
+    'input[name="delivery_time"]:checked',
+  );
   const timeError = document.getElementById("time-error");
 
-  if (secilenSaat === null) { 
+  if (secilenSaat === null) {
     timeError.style.display = "block";
     formDüzgündür = false;
   } else {
     timeError.style.display = "none";
   }
 
-
-
-
-
   if (formDüzgündür === true) {
     if (confirm("Sifaris etmek ucun eminsiniz?")) {
       alert("Sifarisiniz ugurla qebul edildi!");
 
-      let commentValue = document.getElementById("comments").value
-      
+      let commentValue = document.getElementById("comments").value;
+
       let order = {
         product: productXana.input.value,
         quantity: quantityXana.input.value,
